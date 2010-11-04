afb8843e65508fe54d5818e114fd62a5e73eceee
Masahiro Nagano <kazeburo@gmail.com>
2010-11-04 23:39:49 +0900

    initial commit

---
+++ lib/Plack/Middleware/Log/Minimal.pm
@@ -0,0 +1,32 @@
+package Plack::Middleware::Log::Minimal;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::Middleware::Log::Minimal -
+
+=head1 SYNOPSIS
+
+  use Plack::Middleware::Log::Minimal;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::Log::Minimal is
+
+=head1 AUTHOR
+
+Masahiro Nagano E<lt>kazeburo {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
